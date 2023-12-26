dbe1b02de7c12469b8a849be9d8691bebf5692dc
Arman <iperov.55@gmail.com>
2023-12-26 20:23:08 +0400

    v1

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import Header from "./components/Header";
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+};
+
+export default App;
+
