23f268160908143e47cb9264706909bf283fd6b7
Arman <iperov.55@gmail.com>
2024-02-13 16:56:55 +0400

    test by gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,36 @@
- 
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello programmers!!!");
+})
+
+app.post('/auth/login', (req, res) =>{
+    console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Arman Aristakesyan',
+        password: req.body.password,
+    }, 
+    'secret123'    
+    );
+
+    res.json({
+        success: true,
+        token, 
+    });
+
+});
+
+app.listen(4444, (err) => {
+    if(err){
+        console.log(err);
+    }
+
+    console.log('Server 200 OK!!!');
+})
+
